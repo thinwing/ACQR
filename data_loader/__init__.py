6a01cd2954d984bc8c2a504709dd1fd2ce301019
thinwing <150758265+thinwing@users.noreply.github.com>
2023-12-29 14:52:55 +0900

    Add files via upload

--- data_loader/__init__.py
+++ data_loader/__init__.py
@@ -3,5 +3,8 @@ from .data_create2 import dt_create as dt_data2
 from .data_noise import noise_create as dt_noise
 from .data_outlier import outlier_create as dt_outlier
 from .data_outlier2 import outlier_create2 as dt_outlier2
+from .data_outlier3 import outlier_create_lo as dt_outlierlo
+from .data_outlier3 import outlier_create_hi as dt_outlierhi
+from .data_outlier3 import outlier_create_hal as dt_outlierhal
 
-__all__ = ['dt_data', 'dt_data2', 'dt_noise',  'dt_outlier', 'dt_outlier2']
+__all__ = ['dt_data', 'dt_data2', 'dt_noise',  'dt_outlier', 'dt_outlier2', 'dt_outlierlo', 'dt_outlierhi', 'dt_outlierhal']
